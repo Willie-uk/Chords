d51735f9fe68f59831776ce7956aeed5a2d4f7ce
Kennedy Willie <willyken.uk@gmail.com>
2025-08-15 17:56:53 +0300

    change port

---
+++ src/components/Module3.tsx
@@ -0,0 +1,24 @@
+import "../App.css";
+function Module3() {
+  return (
+    <div className="container my-5">
+      <div className="row">
+        <h3 className="main-title">Piano Lessons</h3>
+        <h1 className="module-title">Module 3</h1>
+        <div className="d-flex justify-content-center mt-4">
+          <h4 className="fw-bold">Content: </h4>
+          <h4>1. Scales: {""}</h4>
+          <div className="ms-2">
+            <h4>a. Major Scale</h4>
+            <h4>b. Minor Scale</h4>
+            <h4>c. Harmonic Minor Scale</h4>
+            <h4>d. Pentatonic Scale</h4>
+            <h4>e. Blues Scale</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Module3;
