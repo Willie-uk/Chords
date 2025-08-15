917a12f6bd82cc7d318fd7d16ea484589b277e27
Kennedy Willie <willyken.uk@gmail.com>
2025-08-15 18:52:47 +0300

    change logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import Intro from "./components/Intro";
 import Module2 from "./components/Module2";
+import { Loader } from "lucide-react";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <Loader className="animate-spin-loader" size={25} />
+      </div>
+    );
+  }
+
   return (
     <>
       <Intro />
